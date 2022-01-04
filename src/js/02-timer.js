66f6f866acc361a4c30fbf655016165a830a49a2
Pirlovski <87051249+Pirlovski@users.noreply.github.com>
2022-01-05 00:04:06 +0300

    add
    add

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -15,99 +15,72 @@ const dataSeconds = document.querySelector('[data-seconds]');
 // console.log(dataEl) ;
 
 startBtn.disabled = true;
-const timerId = null ;
-let dataEl = null ;
+const timerId = null;
+let dataEl = null;
 function changeDate(dataTime) {
-    startBtn.disabled = true;
-    const date = new Date();
-    
-    console.log(dataTime) ;
-    if (date > dataTime) {
-        
-        Notiflix.Notify.failure("Please choose a date in the future");
-             
-    } 
-    
-    startBtn.disabled = false;
-    
-    
-    dataEl = dataTime.getTime();
-    //  return dataEl;
-    
+  startBtn.disabled = true;
+  const date = new Date();
 
+  console.log(dataTime);
+  if (date > dataTime) {
+    Notiflix.Notify.failure('Please choose a date in the future');
+  }
+
+  startBtn.disabled = false;
+
+  dataEl = dataTime.getTime();
+  //  return dataEl;
 }
 
-
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-        changeDate(selectedDates[0]);
-   
-        
-    },
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    changeDate(selectedDates[0]);
+  },
 };
 
 flatpickr(inputEl, options);
 
+startBtn.addEventListener('click', () => {
+  // console.log( dataEl);
+  // console.log(newData);
 
-
-startBtn.addEventListener("click" , ( ) => {
-    
-    // console.log( dataEl);
-    // console.log(newData);
-    
- timerId = setInterval(() => {
-        
-        const newData = new Date().getTime() ;
- const deltaTime = dataEl - newData ;
- if(deltaTime < 0 ) {
-     clearInterval(timerId);
-     
+  timerId = setInterval(() => {
+    const newData = new Date().getTime();
+    const deltaTime = dataEl - newData;
+    if (deltaTime < 0) {
+      clearInterval(timerId);
     }
     const convert = convertMs(deltaTime);
     dataDays.textContent = addLeadingZero(convert.days);
     dataHours.textContent = addLeadingZero(convert.hours);
     dataMinutes.textContent = addLeadingZero(convert.minutes);
     dataSeconds.textContent = addLeadingZero(convert.seconds);
-                //    console.log(deltaTime)
-}, 1000);
+    //    console.log(deltaTime)
+  }, 1000);
 
-function addLeadingZero(value) {
-    return String(value).padStart(2 , "0") ;
-}
-
-
-})
-
+  function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+  }
+});
 
 function convertMs(ms) {
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    const days = Math.floor(ms / day);
-    const hours = Math.floor((ms % day) / hour);
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
-  }
-  
-  console.log(convertMs(2000)); 
-  console.log(convertMs(140000)); 
-  console.log(convertMs(24140000)); 
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
+  return { days, hours, minutes, seconds };
+}
 
-
-
-
-
-
-
-
-
+console.log(convertMs(2000));
+console.log(convertMs(140000));
+console.log(convertMs(24140000));
